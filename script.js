b6bff8ad4b3b44a8bfc0023f5db4b5304a75bd13
Kim Hardy <kim@acolyte.ws>
2021-10-12 12:31:12 -0400

    wip

--- script.js
+++ script.js
@@ -69,14 +69,59 @@ const displayMovements = function(movements) {
         const html = `
             <div class="movements__row">
                 <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
-                <div class="movements__value">${mov}</div>
+                <div class="movements__value">${mov} €</div>
             </div>
         `;
         containerMovements.insertAdjacentHTML('afterbegin', html);
     });
 };
-
 displayMovements(account1.movements);
+
+const calcDisplayBalance = function(movements) {
+    const balance = movements
+        .reduce((acc, mov) => acc + mov, 0);
+    labelBalance.textContent = `${balance} €`;
+}
+calcDisplayBalance(account1.movements);
+
+const calcDisplaySummary = function (movements) {
+    const incomes = movements
+        .filter(mov =>  mov > 0)
+        .reduce((acc, mov) => acc + mov, 0);
+    labelSumIn.textContent = `${incomes}€`;
+
+    const out = movements
+        .filter(mov => mov < 0)
+        .reduce((acc, mov) => acc + mov, 0);
+    labelSumOut.textContent = `${Math.abs(out)}€`;
+
+    const interest = movements
+        .filter(mov =>  mov > 0)
+        .map(deposit => deposit * 1.2/100)
+        .filter((int, i, arr) => {
+            return int >= 1;
+        })
+        .reduce((acc, int) => acc + int, 0);
+    labelSumInterest.textContent = `${interest}€`;
+};
+calcDisplaySummary(account1.movements);
+
+// Creating usernames for accounts with the name (initials)
+const createUsernames = function(accs) {
+    accs.forEach(function(acc) {
+        acc.username = acc.owner
+        .toLowerCase()
+        .split(' ')
+        .map(name => name[0])
+        .join('');
+    });
+};
+
+createUsernames(accounts);
+
+
+
+
 
 
 /////////////////////////////////////////////////
