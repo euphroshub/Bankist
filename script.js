b2149ef0066f7fc0ba0859395e72541b3be7b856
euphroshub <euphros@protonmail.com>
2021-10-27 21:11:03 -0400

    sorting implementation

--- script.js
+++ script.js
@@ -60,10 +60,12 @@ const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
 
-const displayMovements = function(movements) {
+const displayMovements = function(movements, sort = false) {
     containerMovements.innerHTML = '';
 
-    movements.forEach(function(mov, i) {
+    const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+    movs.forEach(function(mov, i) {
         const type = mov > 0 ? 'deposit' : 'withdrawal';
 
         const html = `
@@ -208,7 +210,14 @@ btnClose.addEventListener('click', function(e) {
     inputCloseUsername.value = inputClosePin.value = '';
 });
 
+// Sorting movements
+let sorted = false;
 
+btnSort.addEventListener('click', function(e) {
+    e.preventDefault();
+    displayMovements(currentAccount.movements, !sorted);
+    sorted = !sorted;
+});
 
 
 /////////////////////////////////////////////////
